543fc2f7a625cd8730c1d53ac52c3dc5fc44e6bb
DustinRs <drohrschneider@web.de>
2024-01-17 08:28:31 +0100

    commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { MainPageComponent } from './main-page/main-page.component';
 import { ImpressumComponent } from './impressum/impressum.component';
+import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component';
 
 export const routes: Routes = [
     { path: '', component: MainPageComponent},
-    { path: 'impressum', component: ImpressumComponent}
+    { path: 'impressum', component: ImpressumComponent},
+    { path:'privacyPolicy', component: PrivacyPolicyComponent}
 ];
