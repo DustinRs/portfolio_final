199d6984e0d94ea0984fe2f5067e9fedf5d3497c
DustinRs <drohrschneider@web.de>
2024-07-02 01:06:38 +0200

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -8,7 +8,15 @@ import { SkillsComponent } from './skills/skills.component';
 import { PortfolioComponent } from './portfolio/portfolio.component';
 import { ContactComponent } from './contact/contact.component';
 import { FooterComponent } from './footer/footer.component';
-
+import { Observable } from 'rxjs';
+import {
+  Firestore,
+  collection,
+  collectionData,
+  doc,
+  getDocs,
+  setDoc,
+} from '@angular/fire/firestore';
 
 @Component({
   selector: 'app-root',
@@ -29,4 +37,31 @@ import { FooterComponent } from './footer/footer.component';
 })
 export class AppComponent {
   title = 'portfolio';
+  firestore: Firestore = inject(Firestore);
+  items$: Observable<any[]>;
+  date: Date = new Date();
+
+  constructor() {
+    const aCollection = collection(this.firestore, 'Besucher');
+    this.items$ = collectionData(aCollection);
+  }
+
+  async ngOnInit() {
+    let id = Date.now().toString();
+    const options: Intl.DateTimeFormatOptions = {
+      weekday: 'long',
+      day: '2-digit',
+      month: '2-digit',
+      year: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+    };
+    const formattedDate: string = new Intl.DateTimeFormat(
+      'de-DE',
+      options
+    ).format(this.date);
+    await setDoc(doc(this.firestore, 'Besucher', id), {
+      Besucht_am: formattedDate,
+    });
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,23 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations()]
+  providers: [provideRouter(routes), provideAnimations(),importProvidersFrom(
+    provideFirebaseApp(() =>
+      initializeApp({
+        apiKey: "AIzaSyA_vBp-eFxKbgcImdw7-hf2BUjymYNGExA",
+  authDomain: "portfolio-56e59.firebaseapp.com",
+  projectId: "portfolio-56e59",
+  storageBucket: "portfolio-56e59.appspot.com",
+  messagingSenderId: "1070337356847",
+  appId: "1:1070337356847:web:6085efb75f75be70ecc2da"
+      })
+    )
+
+  ),importProvidersFrom(provideFirestore(() => getFirestore())),]
 };
